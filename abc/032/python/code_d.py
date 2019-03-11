f95f6e855ad24bbc6b1031bb5834673b01f6d941
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-03-12 01:11:47 +0900

    hoge

---
+++ abc/032/python/code_d.py
@@ -0,0 +1,20 @@
+N, W = map(int, input().split())
+items = []
+for _ in range(N):
+    w, v = map(int, input().split())
+    items.append({"w": w, "v": v})
+
+dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+
+for i in range(N):
+    item_w = items[i]["w"]
+    item_v = items[i]["v"]
+    for w in range(W + 1):
+        if w >= item_w:
+            dp[i + 1][w] = max(dp[i][w - item_w] + item_v, dp[i][w])
+        else:
+            dp[i + 1][w] = dp[i][w]
+
+
+print(dp[N][W])
